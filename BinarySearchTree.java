7e81b528b473020e13fb9eb93f79fea0cfb0619e
rjsgkw <59462975+rjsgkw@users.noreply.github.com>
2020-03-21 10:10:41 +1100

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -5,7 +5,6 @@ However, a Map is created to keep track of duplicates.
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
